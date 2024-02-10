ccf1bddc7fa42f3c1b26563f0ceb7420c97bbfda
Lucas Silva <100864919+HiLucasSilva@users.noreply.github.com>
2024-02-10 13:37:47 -0300

    teste

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,6 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
-#from src.controllers.tag_creator_controller import TagCreatorController
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
@@ -8,14 +8,13 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        #tag_creator_controller = TagCreatorController()
+        tag_creator_controller = TagCreatorController()
 
         body = http_request.body
         product_code = body["product_code"]
 
         # logica de regra de negocio
-        print('Estou na minha view')
-        #formatted_response = tag_creator_controller.create(product_code)
+        formatted_response = tag_creator_controller.create(product_code)
 
         # retorno http
-        return HttpResponse(status_code=200, body={"hello": "Heew"})
+        return HttpResponse(status_code=200, body=formatted_response)
